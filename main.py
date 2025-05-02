f2138dbce6020f2ceed673d010400dc8ba9a7a41
Kyle Fuller <gitkyle711@gmail.com>
2025-05-02 07:33:06 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,99 @@
+import os
+import requests
+import json
+
+from datetime import date, timedelta
+
+API_KEY = "NjdmNDY1ODUwZjFmMmQ3OGY2M2UzM2YwLjQ3RTZFQTg1Qzk0RDQ4QkM5MkVBQ0JGRDI1OEY5NUM2"
+URL = "https://rest.method.me/api/v1"
+ROOT_FOLDER = 'pm-checklists'
+
+previous_day = ""
+current_day = ""
+
+skip_amount = 0
+
+daily_work_orders_list = []
+
+file_id_request = f"{URL}/files?table=Activity&recordId="
+
+work_order_info_request = f"{URL}/tables/Activity/"
+
+daily_completed_work_orders_request = (f"{URL}/tables/Activity?skip={skip_amount}&top=100&filter=ActualCompletedDate "
+                                       f"ge "
+                                       f"'{previous_day}T00:00:00' "
+                                       f"and ActualCompletedDate lt '{current_day}T00:00:00'&fields=RecordID,"
+                                       f"ActualCompletedDate,ResourceHolder")
+
+headers = {'Authorization': f'APIKey {API_KEY}'}
+
+
+"""    if work_order['Comments'] == 'PWD:PM':
+        daily_completed_work_orders.append(work_order['RecordID'])
+
+for work_order in daily_completed_work_orders:
+    file_id = requests.request('GET', f"{file_id_request}{work_order}", headers=headers)
+    file_id_list.append(file_id.text)
+"""
+
+class ServiceTicket:
+    def __init__(self, record_id, customer, comments):
+        self.work_order_num = record_id
+        self.customer = customer
+        self.comments = comments
+        self.file_id = None
+
+        self._set_file_id()
+
+    def __repr__(self):
+        return (f"WORK ORDER NUMBER: {self.work_order_num}\n"
+                f"CUSTOMER NAME: {self.customer}\n"
+                f"WORK ORDER TYPE: {self.comments}\n"
+                f"FILE ID: {self.file_id}\n\n")
+
+    def _set_file_id(self):
+        self.file_id = requests.request('GET', f"{file_id_request}{self.work_order_num}", headers=headers)
+
+
+def request_daily_work_orders():
+    print("Requesting work orders...")
+    response = requests.request("GET", daily_completed_work_orders_request, headers=headers)
+    print(f"Response status code: {response.status_code}")
+    sample_data = json.loads(response.text)
+    print("SAMPLE DATA TYPE:", type(sample_data))
+    return sample_data
+
+
+def initialize_storage_folder(parent_dir=ROOT_FOLDER):
+    if not os.path.exists(parent_dir):
+        print("Creating checklists folder...")
+        os.mkdir(parent_dir)
+
+
+def initialize_customer_folders(work_orders: list[dict], parent_dir=ROOT_FOLDER):
+    for work_order in work_orders:
+        if not os.path.exists(work_order['Customer']):
+            os.mkdir(os.path.join(parent_dir, work_order['Customer']))
+
+
+def set_today() -> str:
+    today = date.today()
+    date_string = f"{today.year}-{today.month}-{today.day}"
+    return date_string
+
+
+def set_yesterday() -> str:
+    today = date.today()
+    delta = timedelta(days=1)
+    yesterday = today - delta
+    date_string = f"{yesterday.year}-{yesterday.month}-{yesterday.day}"
+    return date_string
+
+
+if __name__ == '__main__':
+    initialize_storage_folder()
+
+    current_day = set_today()
+    previous_day = set_yesterday()
+
+    #initialize_customer_folders()
